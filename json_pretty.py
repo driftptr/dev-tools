a94c8fab92e7af2271fafd1e254fd67f44fe24ba
driftptr <driftptr@proton.me>
2026-04-28 21:05:00 +0200

    json_pretty: add stdin support

--- json_pretty.py
+++ json_pretty.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""Pretty-print JSON files."""
+"""Pretty-print JSON from a file or stdin."""
 
 import json
 import sys
 
 
 def main() -> None:
-    if len(sys.argv) < 2:
-        print("usage: json_pretty.py <file.json>", file=sys.stderr)
-        sys.exit(1)
-    with open(sys.argv[1], encoding="utf-8") as fh:
-        data = json.load(fh)
+    if len(sys.argv) >= 2:
+        with open(sys.argv[1], encoding="utf-8") as fh:
+            data = json.load(fh)
+    else:
+        data = json.load(sys.stdin)
     print(json.dumps(data, indent=2, ensure_ascii=False))
 
 
